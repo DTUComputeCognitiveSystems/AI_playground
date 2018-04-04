59fb750d1a198d9c5bb87729f5fe6d7ed106d0f8
Jeppe <jepno@dtu.dk>
2018-04-04 11:03:37 +0200

    Simple methods for fetching images from webcam with OpenCV and showing them in matplotlib.

---
+++ src/image/capture_webcam.py
@@ -0,0 +1,70 @@
+import cv2
+import matplotlib
+from matplotlib import image
+import numpy as np
+
+matplotlib.use('TkAgg')
+from matplotlib import animation
+import matplotlib.pyplot as plt
+from functools import lru_cache
+
+
+@lru_cache(maxsize=1)
+def get_capturer():
+    """
+    Gets a video/photo capturer from cv2 and returns the same object every time.
+    :rtype: cv2.VideoCapture
+    """
+    return cv2.VideoCapture(0)
+
+
+def get_photo(photo_capturer=None):
+    """
+    Fetches a photo from webcam.
+    :param cv2.VideoCapture photo_capturer:
+    :rtype: np.ndarray
+    """
+    if photo_capturer is None:
+        photo_capturer = get_capturer()
+
+    # Read and wait
+    _, out = photo_capturer.read()
+    while out is None:
+        pass
+
+    return out
+
+
+class Video:
+    def __init__(self, fig=None):
+
+        if fig is None:
+            fig = plt.figure()
+
+        # Initialize image plot
+        frame = get_photo()
+        image_plot = plt.imshow(frame)  # type: image.AxesImage
+        plt.draw()
+        plt.show()
+
+        def init():
+            image_plot.set_data(frame)
+
+        def animate(i):
+            frame = get_photo()
+            image_plot.set_data(frame)
+
+            return image_plot
+
+        self.anim = animation.FuncAnimation(
+            fig,
+            animate,
+            init_func=init,
+            interval=50
+        )
+
+
+if __name__ == "__main__":
+    plt.close("all")
+    plt.ion()
+    the_video = Video()

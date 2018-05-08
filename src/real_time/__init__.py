5e8cdba96d6fb37adbd92336d401ac37b3e24358
Jeppe Nørregaard <jepno@dtu.dk>
2018-05-08 13:37:50 +0200

    __init__.py file to the real-time systems.

---
+++ src/real_time/__init__.py
@@ -0,0 +1,4 @@
+from src.real_time.base_backend import BackendInterfaceObject, BackendInterface
+from src.real_time.background_backend import BackgroundLoop
+from src.real_time.matplotlib_backend import MatplotlibLoop
+from src.real_time.text_input_backend import TextInputLoop

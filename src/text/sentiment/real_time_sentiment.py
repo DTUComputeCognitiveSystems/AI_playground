3ac52993ffd0e7845ee9517742e7dce17956405b
Jeppe Nørregaard <jepno@dtu.dk>
2018-05-18 11:00:12 +0200

    Sentiment highlighting split into two files for ease of access.

--- src/text/sentiment/real_time_sentiment.py
+++ src/text/sentiment/real_time_sentiment.py
@@ -1,52 +1,14 @@
 from copy import deepcopy
+from threading import Timer
 
 import matplotlib.pyplot as plt
-import numpy as np
 from afinn import Afinn
 from matplotlib.figure import Figure
 
-from threading import Timer
-
 from src.real_time.base_backend import BackendInterface
 from src.real_time.text_input_backend import TextInputLoop
-from src.text.utility.text_modifiers import TextModifier
+from src.text.sentiment.sentiment_highlighting import sentiment_text_modifiers
 from src.text.utility.text_plots import flow_text_into_axes
-
-_sentiment_max = 5
-
-
-_sentiment_styles = {
-    1: (None, None, 0.5),
-    2: (None, None, 0.75),
-    3: (None, None, 1.0),
-    4: (None, "italic", 1.0),
-    5: ("bold", "italic", 1.0),
-}
-
-
-__positive_test_words = "yes sweet great fantastic superb"
-__negative_test_words = "no damn bad fraud prick"
-
-
-def _sentiment_format(sentiment, full_contrast):
-    # Get sign
-    sign = np.sign(sentiment)
-
-    # Determine formatting
-    if full_contrast:
-        weight = "bold"
-        style = None
-        color_val = 1.0
-    else:
-        weight, style, color_val = _sentiment_styles[int(abs(sentiment))]
-    color_val = sign * color_val
-
-    # Compute color
-    color = np.array([0., 0., 0.])
-    color[0] -= min(color_val, 0)
-    color[2] += max(color_val, 0)
-
-    return color, weight, style
 
 
 class SentimentHighlighter(BackendInterface):
@@ -56,7 +18,6 @@ class SentimentHighlighter(BackendInterface):
         self.remove_axes = remove_axes
         self.n_lines = lines_in_view
         self.backend = backend  # type: TextInputLoop
-        self.afinn = Afinn()
         self.resized_timer = None
 
         # For options in widgets
@@ -92,37 +53,6 @@ class SentimentHighlighter(BackendInterface):
         # If canvas is resised - then redraw
         self.canvas.mpl_connect("resize_event", self._note_resize)
 
-    def make_modifiers(self, text):
-        # Get sentiment-words and their sentiments
-        sentiment_words = self.afinn.find_all(text)
-        sentiments = [self.afinn.score(word) for word in sentiment_words]
-
-        # Make modifiers
-        modifiers = []
-        idx = 0
-        for word, sentiment in zip(sentiment_words, sentiments):
-
-            # Next index
-            idx = text[idx:].find(word) + idx
-
-            # End position of word
-            end = idx + len(word)
-
-            # Determine format
-            color, weight, style = _sentiment_format(sentiment=sentiment, full_contrast=self.full_contrast)
-
-            # Add modifier
-            modifiers.append(TextModifier(idx, end, "color", color))
-            if weight is not None:
-                modifiers.append(TextModifier(idx, end, "weight", weight))
-            if style is not None:
-                modifiers.append(TextModifier(idx, end, "style", style))
-
-            # Next
-            idx = end
-
-        return modifiers
-
     def _loop_step(self, _=None):
         self.resized_timer = None
 
@@ -144,7 +74,7 @@ class SentimentHighlighter(BackendInterface):
 
             # Make modifiers
             if self.do_highlighting:
-                self.c_modifiers = self.make_modifiers(text=self.c_text)
+                self.c_modifiers = sentiment_text_modifiers(text=self.c_text, full_contrast=self.full_contrast)
             else:
                 self.c_modifiers = []
 
---
+++ src/text/sentiment/sentiment_highlighting.py
@@ -0,0 +1,89 @@
+from functools import lru_cache
+
+import numpy as np
+from afinn import Afinn
+
+from src.text.utility.text_modifiers import TextModifier
+
+_sentiment_max = 5
+
+
+_sentiment_styles = {
+    1: (None, None, 0.5),
+    2: (None, None, 0.75),
+    3: (None, None, 1.0),
+    4: (None, "italic", 1.0),
+    5: ("bold", "italic", 1.0),
+}
+
+
+__positive_test_words = "yes sweet great fantastic superb"
+__negative_test_words = "no damn bad fraud prick"
+
+
+@lru_cache(maxsize=2)
+def _get_afinn():
+    return Afinn()
+
+
+def _sentiment_format(sentiment, full_contrast):
+    # Get sign
+    sign = np.sign(sentiment)
+
+    # Determine formatting
+    if full_contrast:
+        weight = "bold"
+        style = None
+        color_val = 1.0
+    else:
+        weight, style, color_val = _sentiment_styles[int(abs(sentiment))]
+    color_val = sign * color_val
+
+    # Compute color
+    color = np.array([0., 0., 0.])
+    color[0] -= min(color_val, 0)
+    color[2] += max(color_val, 0)
+
+    return color, weight, style
+
+
+def get_sentiment_words(text):
+    # Get AFinn
+    afinn = _get_afinn()
+
+    # Get sentiment-words and their sentiments
+    sentiment_words = afinn.find_all(text)
+    sentiments = [afinn.score(word) for word in sentiment_words]
+
+    return sentiments, sentiment_words
+
+
+def sentiment_text_modifiers(text, full_contrast=False):
+    # Get sentiments
+    sentiments, sentiment_words = get_sentiment_words(text=text)
+
+    # Make modifiers
+    modifiers = []
+    idx = 0
+    for word, sentiment in zip(sentiment_words, sentiments):
+
+        # Next index
+        idx = text[idx:].find(word) + idx
+
+        # End position of word
+        end = idx + len(word)
+
+        # Determine format
+        color, weight, style = _sentiment_format(sentiment=sentiment, full_contrast=full_contrast)
+
+        # Add modifier
+        modifiers.append(TextModifier(idx, end, "color", color))
+        if weight is not None:
+            modifiers.append(TextModifier(idx, end, "weight", weight))
+        if style is not None:
+            modifiers.append(TextModifier(idx, end, "style", style))
+
+        # Next
+        idx = end
+
+    return modifiers

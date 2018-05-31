a00cac77e8ee92de4c73c302802e842a67dae9ec
Jeppe Nørregaard <jepno@dtu.dk>
2018-05-31 14:36:17 +0200

    Upgrade to document embedding visualisation.

--- notebooks/exercises/src/text/fasttext_document_visualisation.py
+++ notebooks/exercises/src/text/fasttext_document_visualisation.py
@@ -6,9 +6,13 @@ from mpl_toolkits.mplot3d import axes3d, Axes3D
 
 from IPython.display import display, clear_output
 from fastText import FastText
-from ipywidgets import Tab, Text, Checkbox, Label, HBox, VBox, Button, Dropdown
+from ipywidgets import Tab, Text, Checkbox, Label, HBox, VBox, Button, Dropdown, Textarea
 import wikipedia
 from sklearn.decomposition import PCA
+
+
+# TODO: Make system fir handling DisambiguationError when searching wikipedia
+
 
 _default_wikipedia_articles = [
     "Duck",
@@ -35,6 +39,8 @@ _checkbox_layout = dict(width="30pt")
 _label_layout = dict(width="180pt")
 _remove_layout = dict(width="60pt")
 _additional_info_layout = dict(width="180pt", padding="0pt 0pt 0pt 20pt")
+_editable_text_layout = dict(width="350pt")
+_editable_label_layout = dict(width="50pt")
 
 
 @lru_cache(maxsize=1000)
@@ -71,20 +77,71 @@ class Document:
         raise NotImplementedError
 
 
+class EditableDocument(Document):
+    n_rows = 0
+
+    def __init__(self):
+        EditableDocument.n_rows += 1
+        self._name = "T{}".format(EditableDocument.n_rows)
+        self._text = Textarea(
+            value='',
+            placeholder='Type something',
+            description='',
+            disabled=False,
+            layout=_editable_text_layout,
+        )
+        self.label = Label(value=self._name, layout=_editable_label_layout)
+        self.checkbox = Checkbox(value=True, layout=_checkbox_layout, indent=False)
+        self.remove_button = Button(
+            description='Remove',
+            disabled=False,
+            button_style='danger',  # 'success', 'info', 'warning', 'danger' or ''
+            layout=_remove_layout,
+        )
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def text(self):
+        return self._text.value
+
+    @property
+    def on(self):
+        return self.checkbox.value and self.text
+
+    def __iter__(self):
+        yield self.checkbox
+        yield self.label
+        yield self._text
+        yield self.remove_button
+
+    @staticmethod
+    def make_header():
+        return HBox(
+            (Label("Use", layout=_checkbox_layout),
+             Label("Name", layout=_editable_label_layout),
+             Label("Text", layout=_editable_text_layout),
+             Label("Remove", layout=_remove_layout),
+             )
+        )
+
+
 class WikipediaPageRow(Document):
     def __init__(self, name=None):
         self.editable = name is None
         self.checkbox = Checkbox(value=True, layout=_checkbox_layout, indent=False)
-        self.true_name = self.summary = None
+        self.true_name = self.summary = self.last_textfield = None
 
         if self.editable:
             self.text_field = Text(layout=_label_layout)
         else:
             self.text_field = Label(value=name, layout=_label_layout)
 
-        self.button = Button(
+        self.remove_button = Button(
             description='Remove',
-            disabled=False,
+            disabled=False,b
             button_style='danger',  # 'success', 'info', 'warning', 'danger' or ''
             layout=_remove_layout,
         )
@@ -93,22 +150,23 @@ class WikipediaPageRow(Document):
     def __iter__(self):
         yield self.checkbox
         yield self.text_field
-        yield self.button
+        yield self.remove_button
         yield self.additional_info
 
     def _search(self):
-        if self.true_name is None:
-            self.true_name = _get_wikipedia_search(name=self.name)[0]
+        if self.last_textfield != self.text_field_value:
+            self.last_textfield = self.text_field_value
+            self.true_name = _get_wikipedia_search(name=self.text_field_value)[0]
             self.summary = _get_wikipedia_summary(self.true_name)
 
     @property
-    def searched_name(self):
-        self._search()
-        return self.true_name
+    def text_field_value(self):
+        return self.text_field.value
 
     @property
     def name(self):
-        return self.text_field.value
+        self._search()
+        return self.true_name
 
     @property
     def text(self):
@@ -117,35 +175,41 @@ class WikipediaPageRow(Document):
 
     @property
     def on(self):
-        return self.checkbox.value and self.name
+        return self.checkbox.value and self.text_field_value
 
-
-class WikipediaPagesTab:
-    def __init__(self):
-        self.observers = []
-
-        # Make a header
-        self._header = HBox(
+    @staticmethod
+    def make_header():
+        return HBox(
             (Label("Use", layout=_checkbox_layout),
              Label("Page Search Name", layout=_label_layout),
              Label("Remove", layout=_remove_layout),
              Label("Search Result", layout=_additional_info_layout))
         )
 
-        # Wikipedia pages
-        self._wikipedia_pages = [WikipediaPageRow(name=val) for val in _default_wikipedia_articles]
+
+class DocumentRowTab:
+    def __init__(self, title, row_type, initial_content=None):
+        self.row_type = row_type
+        self.title = title
+        self.observers = []
+
+        # Make a header
+        self._header = self.row_type.make_header()
+
+        # Rows
+        self._rows = initial_content if initial_content is not None else []
 
         # Observe remove buttons
-        for page in self._wikipedia_pages:
-            page.button.on_click(self._remove_page)
+        for page in self._rows:
+            page.remove_button.on_click(self._remove_page)
 
-        # Button for adding a new page
-        self._new_page_button = Button(
-            description='New page',
+        # Button for adding a new row
+        self._new_row_button = Button(
+            description='New row',
             disabled=False,
             button_style='info',  # 'success', 'info', 'warning', 'danger' or ''
         )
-        self._new_page_button.on_click(self._add_page)
+        self._new_row_button.on_click(self._add_page)
 
     def observe(self, func):
         self.observers.append(func)
@@ -155,43 +219,39 @@ class WikipediaPagesTab:
             observer()
 
     @property
-    def pages(self):
-        return self._wikipedia_pages
+    def rows(self):
+        return self._rows
 
     @property
-    def n_pages(self):
-        return len(self._wikipedia_pages)
+    def n_rows(self):
+        return len(self._rows)
 
     @property
-    def tab(self):
-        tab = Tab()
-        tab.children = [
-            VBox(
-                (self._header,) +
-                tuple(HBox(tuple(val)) for val in self._wikipedia_pages) +
-                (HBox((Label("", layout=dict(width="60pt")), self._new_page_button)),)
-            )
-        ]
-        tab.set_title(0, "Wikipedia Pages")
-        return tab
+    def tab_child(self):
+        child = VBox(
+            (self._header,) +
+            tuple(HBox(tuple(val)) for val in self._rows) +
+            (HBox((Label("", layout=dict(width="60pt")), self._new_row_button)),)
+        )
+        return child
 
     def _add_page(self, _=None):
         # Make new page
-        new_row = WikipediaPageRow()
-        new_row.button.on_click(self._remove_page)
+        new_row = self.row_type()
+        new_row.remove_button.on_click(self._remove_page)
 
         # Add
-        self._wikipedia_pages.append(new_row)
+        self._rows.append(new_row)
 
         # Broadcast change
         self._on_change()
 
     def _remove_page(self, button):
         # Find the pressed button
-        nr = next(idx for idx, page in enumerate(self._wikipedia_pages) if page.button == button)
+        nr = next(idx for idx, row in enumerate(self._rows) if row.remove_button == button)
 
         # Remove the nr
-        self._wikipedia_pages = self._wikipedia_pages[:nr] + self._wikipedia_pages[nr + 1:]
+        self._rows = self._rows[:nr] + self._rows[nr + 1:]
 
         # Broadcast change
         self._on_change()
@@ -200,7 +260,7 @@ class WikipediaPagesTab:
 class DocumentEmbeddingVisualiser:
     def __init__(self, fasttext_model, figsize=(8, 6)):
         self.figsize = figsize
-        self.fig = None
+        self.fig = self.tab = None
 
         # Get fastText model
         self.fasttext_model = fasttext_model  # type: FastText._FastText
@@ -224,19 +284,45 @@ class DocumentEmbeddingVisualiser:
         )
 
         # Make Wikipedia-page tab
-        self.wikipedia_tab = WikipediaPagesTab()
+        self.wikipedia_tab = DocumentRowTab(
+            title="Wikipedia Pages",
+            row_type=WikipediaPageRow,
+            initial_content=[WikipediaPageRow(name=val) for val in _default_wikipedia_articles]
+        )
         self.wikipedia_tab.observe(self._display)
+
+        # Free text tab
+        self.free_text_tab = DocumentRowTab(
+            title="Free Text",
+            row_type=EditableDocument,
+            initial_content=[EditableDocument()],
+        )
+        self.free_text_tab.observe(self._display)
 
         self._display()
 
     def _display(self):
         clear_output(wait=True)
 
+        # Get current tab index
+        tab_index = 0
+        if self.tab is not None:
+            tab_index = self.tab.selected_index
+
         # Get Wikipedia tab
-        tab = self.wikipedia_tab.tab
+        self.tab = Tab()
+        self.tab.children = [
+            self.wikipedia_tab.tab_child,
+            self.free_text_tab.tab_child,
+        ]
+        self.tab.set_title(0, self.wikipedia_tab.title)
+        self.tab.set_title(1, self.free_text_tab.title)
+
+        # Select the wanted tab
+        self.tab.selected_index = tab_index
 
         # Go button with tab
-        box = VBox((HBox((self._do_embeddings_button, self._document_vector_type)), tab))
+        box = VBox((HBox((self._do_embeddings_button, self._document_vector_type)), self.tab))
 
         if self.fig is not None:
             # noinspection PyTypeChecker
@@ -251,32 +337,47 @@ class DocumentEmbeddingVisualiser:
         # Disable button
         self._do_embeddings_button.button_style = "warning"
         self._do_embeddings_button.disabled = True
-        self._do_embeddings_button.description = getting_page_formatter.format(0, self.wikipedia_tab.n_pages)
+        self._do_embeddings_button.description = getting_page_formatter.format(0, self.wikipedia_tab.n_rows)
 
-        # Download pages
-        summaries = []
-        summary_names = []
-        for nr, page in enumerate(self.wikipedia_tab.pages):
+        # Initialize
+        document_texts = []
+        document_names = []
+        document_colors = []
+
+        # Go through wikipedia pages
+        for nr, page in enumerate(self.wikipedia_tab.rows):
 
             # Check if page is wanted and valid
             if page.on:
 
                 # Get name and text from wikipedia search
-                summary_names.append(page.searched_name)
-                summaries.append(page.text)
+                document_names.append(page.name)
+                document_texts.append(page.text)
+                document_colors.append("black")
 
                 # Notify
                 self._do_embeddings_button.description = getting_page_formatter \
-                    .format(nr + 1, self.wikipedia_tab.n_pages)
-                page.additional_info.value = "-> " + page.searched_name
+                    .format(nr + 1, self.wikipedia_tab.n_rows)
+                page.additional_info.value = "-> " + page.name
+
+        # Go through free text
+        for nr, page in enumerate(self.free_text_tab.rows):
+
+            # Check if page is wanted and valid
+            if page.on:
+
+                # Get name and text from wikipedia search
+                document_names.append(page.name)
+                document_texts.append(page.text)
+                document_colors.append("blue")
 
         # Check for not enough samples
-        if len(summaries) >= 3:
+        if len(document_texts) >= 3:
 
-            # Go through pages and computer mean word embedding
+            # Compute document vectors
             self._do_embeddings_button.description = "Computing document vectors."
             document_vectors = []
-            for summary in summaries:
+            for summary in document_texts:
                 vector_array = np.array([_get_fasttext_word_embedding(self.fasttext_model, word=word) for word in summary])
 
                 # Compute document embedding with mean
@@ -305,10 +406,11 @@ class DocumentEmbeddingVisualiser:
             self._do_embeddings_button.description = "Plotting projections."
             self.fig = plt.figure(figsize=self.figsize)
             ax = Axes3D(self.fig)
-            for name, vector in zip(summary_names, projections):
+            for name, vector, color in zip(document_names, projections, document_colors):
                 ax.text(
                     *vector,
-                    s=name
+                    s=name,
+                    color=color
                 )
 
             # Set limits

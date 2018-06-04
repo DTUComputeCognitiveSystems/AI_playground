ba3ef2874e70771d8111c7d80af0b0246befe902
Christopher Heje Grønbech <chgroenbech@gmail.com>
2018-06-04 14:31:05 +0200

    Add custom vocabulary to bag of words
    A custom vocabulary can now be set when initialising or later, and this
    is passed to underlying count-vectoriser class from `sci-kit learn`.
    
    For large vocabularies, the count matrix will be very sparse, so when
    converting to a data frame a sparse one is now used. Seaborn has
    unfortunately not been updated to handle this format, so functionality
    to remove absent words in a count matrix has been added and is used
    before converting to a dense data frame.

--- notebooks/exercises/src/text/bag_of_words.py
+++ notebooks/exercises/src/text/bag_of_words.py
@@ -1,26 +1,22 @@
 import numpy
 import pandas
 import seaborn
-# import scipy.sparse
 from matplotlib import pyplot
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
 
 COLOUR_MAP = seaborn.cubehelix_palette(as_cmap=True)
+MAXIMUM_NUMBER_OF_WORDS_WHEN_INCLUDING_ABSENT_ONES = 25
 
 
 class BagOfWords:
-    def __init__(self, corpus, stop_words=[]):
+    def __init__(self, corpus, vocabulary=None, stop_words=None):
 
-        if not isinstance(corpus, list):
-            corpus = [corpus]
+        self.__corpus = ensure_list_input(corpus)
+        self.__vocabulary = ensure_vocabulary_input(vocabulary)
+        self.__stop_words = ensure_list_input(stop_words)
 
-        if not isinstance(stop_words, list):
-            stop_words = [stop_words]
-
-        self.__corpus = corpus
-        self.__stop_words = stop_words
-        self.__count_matrix = self.__tfidf_matrix = self.__vocabulary = None
+        self.__count_matrix = self.__tfidf_matrix = self.__words = None
         self.__number_of_documents = self.__number_of_words = None
 
         self.__update()
@@ -31,9 +27,16 @@ class BagOfWords:
 
     @corpus.setter
     def corpus(self, value):
-        if not isinstance(value, list):
-            value = [value]
-        self.__corpus = value
+        self.__corpus = ensure_list_input(value)
+        self.__update()
+
+    @property
+    def vocabulary(self):
+        return self.__vocabulary
+
+    @vocabulary.setter
+    def vocabulary(self, value):
+        self.__vocabulary = ensure_vocabulary_input(value)
         self.__update()
 
     @property
@@ -42,9 +45,7 @@ class BagOfWords:
 
     @stop_words.setter
     def stop_words(self, value):
-        if not isinstance(value, list):
-            value = [value]
-        self.__stop_words = value
+        self.__stop_words = ensure_list_input(value)
         self.__update()
 
     @property
@@ -52,20 +53,20 @@ class BagOfWords:
         return self.__count_matrix
 
     @property
-    def count_data_frame(self):
-        return self.__as_data_frame(tfidf=False)
+    def count_table(self):
+        return self.__as_data_frame(tfidf=False, remove_absent_words=True)
 
     @property
     def tfidf_matrix(self):
         return self.__tfidf_matrix
 
     @property
-    def tfidf_data_frame(self):
-        return self.__as_data_frame(tfidf=True)
+    def tfidf_table(self):
+        return self.__as_data_frame(tfidf=True, remove_absent_words=True)
 
     @property
-    def vocabulary(self):
-        return self.__vocabulary
+    def words(self):
+        return self.__words
 
     @property
     def number_of_documents(self):
@@ -75,32 +76,54 @@ class BagOfWords:
     def number_of_words(self):
         return self.__number_of_words
 
-    def __as_data_frame(self, tfidf=False):
+    def __as_data_frame(self, tfidf=False, remove_absent_words=False):
 
         if tfidf:
             matrix = self.__tfidf_matrix
         else:
             matrix = self.__count_matrix
 
-        document_number = [i + 1 for i in range(self.__number_of_documents)]
+        document_numbers = [i + 1 for i in range(self.__number_of_documents)]
 
-        data_frame = pandas.DataFrame(
-            matrix.toarray(),
-            index=document_number,
-            columns=self.__vocabulary
-        )
+        data_frame = pandas.SparseDataFrame(
+            matrix,
+            index=document_numbers,
+            columns=self.__words,
+            default_fill_value=0
+        ).astype(matrix.dtype)
+
+        if remove_absent_words:
+            word_sums = data_frame.sum(axis="index")
+            absent_words = word_sums[word_sums == 0].index
+            data_frame = data_frame.drop(columns=absent_words)
 
         return data_frame
 
     def __update(self):
 
-        count_vectoriser = CountVectorizer(stop_words=self.__stop_words)
+        count_vectoriser = CountVectorizer(
+            encoding="utf-8",
+            strip_accents=None,
+            analyzer="word",
+            stop_words=self.__stop_words,
+            lowercase=True,
+            max_df=1.0,
+            min_df=0.0,
+            max_features=None,
+            vocabulary=self.__vocabulary,
+            binary=None
+        )
         tfidf_transformer = TfidfTransformer()
+
+        # Ensure 1-character words are counted if present in vocabulary
+        if self.__vocabulary and min(map(len, self.__vocabulary)) == 1:
+            count_vectoriser.token_pattern = r"(?u)\b\w+\b"
 
         self.__count_matrix = count_vectoriser.fit_transform(self.__corpus)
         self.__tfidf_matrix = tfidf_transformer.fit_transform(
             self.__count_matrix)
-        self.__vocabulary = count_vectoriser.get_feature_names()
+        self.__vocabulary = count_vectoriser.vocabulary_
+        self.__words = count_vectoriser.get_feature_names()
 
         self.__number_of_documents, self.__number_of_words = \
             self.__count_matrix.shape
@@ -114,13 +137,26 @@ class BagOfWords:
                 if filter_(word):
                     stop_words.append(word)
 
-        self.__stop_words += stop_words
+        if self.__stop_words:
+            self.__stop_words += stop_words
+        else:
+            self.__stop_words = stop_words
 
         self.__update()
 
-    def plot(self, tfidf=False):
+    def plot(self, tfidf=False, remove_absent_words=None):
 
-        data_frame = self.__as_data_frame(tfidf=tfidf)
+        if remove_absent_words is None:
+            if self.number_of_words \
+                    > MAXIMUM_NUMBER_OF_WORDS_WHEN_INCLUDING_ABSENT_ONES:
+                remove_absent_words = True
+            else:
+                remove_absent_words = False
+
+        data_frame = self.__as_data_frame(
+            tfidf=tfidf,
+            remove_absent_words=remove_absent_words
+        ).to_dense()
 
         if self.__number_of_documents <= 10 and self.__number_of_words <= 20:
             annotate = True
@@ -146,7 +182,7 @@ class BagOfWords:
             maximum_value = data_frame.values.max()
 
             colour_bar_ticks = numpy.linspace(
-                minimum_value, maximum_value, maximum_value + 1)
+                minimum_value, maximum_value, maximum_value + 2)
 
             colour_bar_parameters["boundaries"] = colour_bar_ticks
             colour_bar_parameters["ticks"] = colour_bar_ticks
@@ -194,3 +230,15 @@ class BagOfWords:
                 axis.get_yticklabels(),
                 rotation="horizontal"
             )
+
+
+def ensure_list_input(value):
+    if value and not isinstance(value, list):
+        value = [value]
+    return value
+
+
+def ensure_vocabulary_input(value):
+    if not isinstance(value, (dict, set)):
+        value = ensure_list_input(value)
+    return value

c0d52ba5ee7057449ed127f0eab85b30e5814217
Antons Fronovics <42849575+fronovics@users.noreply.github.com>
2018-09-10 15:50:27 +0200

    Minor changes

--- src/image/video/labelled.py
+++ src/image/video/labelled.py
@@ -1,10 +1,11 @@
 """ temporary - lines 1-8"""
-import git
-import os
-import sys
-git_root = git.Repo('.', search_parent_directories=True).git.rev_parse("--show-toplevel") # '.' causes issue on windows/osx?
-os.chdir(git_root)
-sys.path.insert(0, git_root)
+if __name__ == "__main__":
+    import git
+    import os
+    import sys
+    git_root = git.Repo('.', search_parent_directories=True).git.rev_parse("--show-toplevel") # '.' causes issue on windows/osx?
+    os.chdir(git_root)
+    sys.path.insert(0, git_root)
 
 import random
 from time import time

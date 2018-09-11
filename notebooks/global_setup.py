d434f00e1b6314725b5fc4fc862072c77e253227
Antons Fronovics <42849575+fronovics@users.noreply.github.com>
2018-09-11 10:30:39 +0200

    GLobal setup add lines

--- notebooks/global_setup.py
+++ notebooks/global_setup.py
@@ -1,5 +1,7 @@
 # Set working direction to project root
 import git
 import os
+import sys
 git_root = git.Repo('.', search_parent_directories=True).git.rev_parse("--show-toplevel") # '.' causes issue on windows/osx?
 os.chdir(git_root)
+sys.path.insert(0, git_root)

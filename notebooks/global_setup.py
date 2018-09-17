e044564472039afcb282e65c2e9c3b2aefb9abe5
Antons Fronovics <42849575+fronovics@users.noreply.github.com>
2018-09-17 15:05:29 +0200

    Logging with Jupyter notebooks, setup without git module

--- notebooks/global_setup.py
+++ notebooks/global_setup.py
@@ -1,22 +1,40 @@
 # Path modules
 import os
 import sys
-import platform
+from pathlib import Path
 # Logging modules
+import platform
 import json
 import logging
 import logging.config
+# Ipython
+from IPython import get_ipython
+import re
 
-# Set working direction to project root
-
-#git_root = git.Repo('.', search_parent_directories=True).git.rev_parse("--show-toplevel") # '.' causes issue on windows/osx?
-root_directory = os.path.realpath("..")
-os.chdir(root_directory)
-sys.path.insert(0, root_directory)
-
-# Set up logging
-
+LOG_FILE_PATH = "errors.log"
 LOG_CONFIG_PATH = "notebooks/logger_config.json"
+
+# Defining functions used in setup
+
+def findRootDir(path, dirname):
+    """
+        Set working directory to project root
+    """
+    path = Path(path)
+    if os.path.basename(path.parent) == dirname:
+        return os.path.realpath(path.parent)
+    else:
+        return findRootDir(path.parent, dirname)
+
+def run_from_ipython():
+    """
+        Detect whether we run the file from iPython or not
+    """
+    try:
+        __IPYTHON__
+        return True
+    except NameError:
+        return False
 
 def setup_logging(
     default_level=logging.INFO):
@@ -24,13 +42,9 @@ def setup_logging(
         Load logging configuration
     """
     global LOG_CONFIG_PATH
-
-    if os.path.exists(LOG_CONFIG_PATH):
-        with open(LOG_CONFIG_PATH, 'rt') as f:
-            config = json.load(f)
-        logging.config.dictConfig(config)
-    else:
-        logging.basicConfig(level=default_level)
+    with open(LOG_CONFIG_PATH, 'rt') as f:
+        config = json.load(f)
+    logging.config.dictConfig(config)
 
 def handle_exception(exc_type, exc_value, exc_traceback):
     """
@@ -42,10 +56,29 @@ def handle_exception(exc_type, exc_value, exc_traceback):
 
     logger.error("Uncaught exception ({} {} {})".format(platform.system(), platform.release(), os.name), exc_info=(exc_type, exc_value, exc_traceback))
 
-# Run setup
-setup_logging()
-# Create logger object
-logger = logging.getLogger("main_logger")
-# Attach the except hook
-sys.excepthook = handle_exception
+# Changing the root directory
+root_directory = findRootDir(os.path.realpath("__file__"), "AI_playground")
+os.chdir(root_directory)
+sys.path.insert(0, root_directory)
+
+# Set up logging
+# If running the script from iPython console, then we redirect the stderr stream to the file. 
+# Else set up logging manually
+if run_from_ipython():
+    # Stream errors into file
+    my_stderr = sys.stderr = open(LOG_FILE_PATH, 'a')
+    ipython = get_ipython()
+    # Remove colors, otherwise log is unreadable
+    ipython.magic("%colors NoColor")
+    logger_formatter = logging.Formatter('%(asctime)s - ({} {} {}) %(name)s - %(levelname)s - %(message)s'.format(platform.system(), platform.release(), os.name))
+    ipython.log.handlers[0].stream = my_stderr
+    ipython.log.handlers[0].setFormatter(logger_formatter)
+    ipython.log.setLevel(logging.INFO)
+else:
+    # Run setup
+    setup_logging()
+    # Create logger object
+    logger = logging.getLogger("main_logger")
+    # Attach the except hook
+    sys.excepthook = handle_exception
 

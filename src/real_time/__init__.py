a8cd424890e1cda59c38aa5f5759713ee9b05ab7
Antons Fronovics <42849575+fronovics@users.noreply.github.com>
2018-09-27 15:05:24 +0200

    Temporarily restored old text-input backend files
    Making text work quickly

--- src/real_time/__init__.py
+++ src/real_time/__init__.py
@@ -1,3 +1,5 @@
 # Refactored architecture
 from src.real_time.frontend_opencv import OpenCVFrontendController
 from src.real_time.frontend_matplotlib import MatplotlibFrontendController
+# Text loop
+from src.real_time.text_input_backend import TextInputLoop

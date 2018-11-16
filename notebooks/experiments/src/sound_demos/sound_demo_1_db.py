f03015bf1068fd09c421a4030d34e87912814a9d
Antons Fronovics <fronovics@gmail.com>
2018-11-16 10:58:44 +0100

    Dashboard development

--- notebooks/experiments/src/sound_demos/sound_demo_1_db.py
+++ notebooks/experiments/src/sound_demos/sound_demo_1_db.py
@@ -2,19 +2,23 @@ from notebooks.experiments.src.sound_demos.multilabel_classifier import Recorder
 from src.audio.mini_recorder import miniRecorder
 from ipywidgets.widgets import Layout, Label, HBox, VBox, HTML, Dropdown, Button, Output
 import os
+import glob
+
+WAV_DIR = "tmp"
+
 
 class SoundDemo1Dashboard1:
     def __init__(self):
         """
         :param int i: 
         """
-        
+
         self.select_training_data_options = None
 
         self.select_training_data = Dropdown(
-            options={'Record training data': 0, 
-                    'Load file ...': 1
-            },
+            options={'Record training data': 0,
+                     'Load file ...': 1
+                     },
             value=0,
             description='Choose training data:',
             disabled=False,
@@ -26,22 +30,22 @@ class SoundDemo1Dashboard1:
 
         self.select_nseconds = Dropdown(
             options={
-                    '2s': 2,
-                    '3s': 3,
-                    '5s': 5
+                '2s': 2,
+                '3s': 3,
+                '5s': 5
             },
             value=2,
             description='Choose recording length:',
             disabled=False,
-            layout=Layout(width='400px', display = 'block'),
+            layout=Layout(width='400px', display='block'),
             style={'description_width': '160px'},
         )
 
         self.select_nclasses = Dropdown(
             options={'2': 2,
-                    '3': 3,
-                    '4': 4
-            },
+                     '3': 3,
+                     '4': 4
+                     },
             value=2,
             description='Choose number of classes:',
             disabled=False,
@@ -50,12 +54,12 @@ class SoundDemo1Dashboard1:
         )
 
         self.select_nfiles = Dropdown(
-            options={'12': 12, 
-                    '8': 8,
-                    '6': 6,
-                    '4': 4,
-                    '2': 2
-            },
+            options={'12': 12,
+                     '8': 8,
+                     '6': 6,
+                     '4': 4,
+                     '2': 2
+                     },
             value=12,
             description='Choose number of files:',
             disabled=False,
@@ -64,7 +68,7 @@ class SoundDemo1Dashboard1:
         )
 
         self.container = Output(
-            value = "",
+            value="",
         )
 
         self.submit_button = Button(
@@ -85,29 +89,27 @@ class SoundDemo1Dashboard1:
             icon=''
         )
 
-        self.widget_box =  VBox(
+        self.widget_box = VBox(
             (
                 HBox(
                     (
-                    VBox((
-                        self.select_training_data, 
-                        self.select_nseconds,
-                        self.select_nclasses, 
-                        self.select_nfiles
-                    ),),
-                    VBox((
-                         self.submit_button,
-                         self.play_button
-                    ),)
+                        VBox((
+                            self.select_training_data,
+                            self.select_nseconds,
+                            self.select_nclasses,
+                            self.select_nfiles
+                        ), ),
+                        VBox((
+                            self.submit_button,
+                            self.play_button
+                        ), )
                     ),
-                ), 
+                ),
                 self.container
             ),
         )
 
         self.submit_button.on_click(self._run)
-
-        
 
     @property
     def start(self):
@@ -121,14 +123,14 @@ class SoundDemo1Dashboard1:
             else:
                 self.select_nseconds.layout.display = 'block'
                 self.select_nseconds.disabled = False
-            #print("changed to {}".format(change['new']))
+            # print("changed to {}".format(change['new']))
 
     def _run(self, v):
         self.prefix = "training"
-        self.recorder = Recorder(n_classes = self.select_nclasses.value, 
-                                 n_files = self.select_nfiles.value, 
-                                 prefix = self.prefix, 
-                                 wav_dir = 'tmp')
+        self.recorder = Recorder(n_classes=self.select_nclasses.value,
+                                 n_files=self.select_nfiles.value,
+                                 prefix=self.prefix,
+                                 wav_dir='tmp')
 
         # If we choose to record the data, then we record it here
         if self.select_training_data.value == 0:
@@ -141,7 +143,7 @@ class SoundDemo1Dashboard1:
             self.recorder.record(seconds=self.select_nseconds.value, clear_output=False)
 
         self.data = self.recorder.create_dataset()
-        return self.widget_box
+        return
 
 
 class SoundDemo1Dashboard2:
@@ -152,9 +154,9 @@ class SoundDemo1Dashboard2:
         self.select_test_data_options = None
 
         self.select_test_data = Dropdown(
-            options={'Record training data': 0, 
-                    'Load file ...': 1
-            },
+            options={'Record training data': 0,
+                     'Load file ...': 1
+                     },
             value=0,
             description='Choose training data:',
             disabled=False,
@@ -166,19 +168,19 @@ class SoundDemo1Dashboard2:
 
         self.select_nseconds = Dropdown(
             options={
-                    '2s': 2,
-                    '3s': 3,
-                    '5s': 5
+                '2s': 2,
+                '3s': 3,
+                '5s': 5
             },
             value=2,
             description='Choose recording length:',
             disabled=False,
-            layout=Layout(width='400px', display = 'block'),
+            layout=Layout(width='400px', display='block'),
             style={'description_width': '160px'},
         )
 
         self.container = Output(
-            value = "",
+            value="",
         )
 
         self.submit_button = Button(
@@ -199,20 +201,20 @@ class SoundDemo1Dashboard2:
             icon=''
         )
 
-        self.widget_box =  VBox(
+        self.widget_box = VBox(
             (
                 HBox(
                     (
-                    VBox((
-                        self.select_test_data, 
-                        self.select_nseconds
-                    ),),
-                    VBox((
-                         self.submit_button,
-                         self.play_button
-                    ),)
+                        VBox((
+                            self.select_test_data,
+                            self.select_nseconds
+                        ), ),
+                        VBox((
+                            self.submit_button,
+                            self.play_button
+                        ), )
                     ),
-                ), 
+                ),
                 self.container
             ),
         )
@@ -231,7 +233,7 @@ class SoundDemo1Dashboard2:
             else:
                 self.select_nseconds.layout.display = 'block'
                 self.select_nseconds.disabled = False
-            #print("changed to {}".format(change['new']))
+            # print("changed to {}".format(change['new']))
 
     def _run(self, v):
         self.prefix = "test"
@@ -239,15 +241,15 @@ class SoundDemo1Dashboard2:
         # If we choose to record the data, then we record it here
         if self.select_test_data.value == 0:
             # Remove the data with the same prefix
-            files = self.recorder.get_files()
+            file_ext = self.prefix + "*.wav"
+            files = glob.glob(os.path.join(WAV_DIR, file_ext))
             for file in files:
                 if self.prefix in file:
                     os.remove(file)
             # Start recording
             rec = miniRecorder(seconds=1.5)
             _ = rec.record()
+            rec.write2file("test.wav")
 
-        #self.data = self.recorder.create_dataset()
-        return self.widget_box
-
-        
+        # self.data = self.recorder.create_dataset()
+        return
